836cd7de3092d5d51085cf6eb8a11dd3ee11bf34
Kaua.Nichioka <73563366+kinguim1000@users.noreply.github.com>
2023-08-10 16:09:12 -0300

    Update ultrasonico.ino

--- ping/ultrasonico.ino
+++ ping/ultrasonico.ino
@@ -1,19 +1,19 @@
 int ultra() {
   
-  pinMode(pinoUltrasonico, OUTPUT);
+  pinMode(pinoUltrasonico, OUTPUT);//torna o pino como saida
   digitalWrite(pinoUltrasonico, LOW);
   delayMicroseconds(2);
   digitalWrite(pinoUltrasonico, HIGH);
   delayMicroseconds(5);
   digitalWrite(pinoUltrasonico, LOW);
-  pinMode(pinoUltrasonico, INPUT);
+  pinMode(pinoUltrasonico, INPUT);//torna o pino como entrada
   
   long duration = pulseIn(pinoUltrasonico, HIGH);
 
   long cm = microsecondsToCentimeters(duration);
   delay(100);
 
-  return cm;
+  return cm; //retorna a distância
 }
 
 
